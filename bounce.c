4d6d733a9e48d73c81acd0c647280d88263bd51f
luketheshack <lmarusha@nd.edu>
2020-11-01 23:02:32 -0500

    part 1 and 2

--- bounce.c
+++ bounce.c
@@ -7,13 +7,14 @@
 int main() {
 	int wd = 500, ht = 400;
 	char c = 0;
-	float xc, yc; // ball x-pos and ball y-pos
-	float vx, vy; // ball velocity x and velocity y
-	int pausetime = 50000;
+	float xc = 0, yc = 0; // ball x-pos and ball y-pos
+	float vx = 0, vy = 0; // ball velocity x and velocity y
+	int pausetime = 20000;
 		
 	gfx_open(wd, ht, "Bouncing Ball");
 	randomizeball(&xc, &yc, &vx, &vy);
-
+	xc = wd/2;
+	yc = ht/2;
 	while (c != 'q') {
 		gfx_clear();
 		gfx_circle(xc, yc, SIZE/2);
@@ -45,6 +46,6 @@ int main() {
 void randomizeball(float *xc, float *yc, float *vx, float *vy) {
 	*(xc) = gfx_xpos();
 	*(yc) = gfx_ypos();
-	*(vx) = (rand() % 10) - 5;
-	*(vy) = (rand() % 10) - 5;
+	*(vx) = (rand() % 12) - 6;
+	*(vy) = (rand() % 12) - 6;
 }
